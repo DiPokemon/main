fc7be6cca28cb57fe126d90096198b8425530f83
DiPokemon <dimitrnklnk@gmail.com>
2022-10-23 23:59:44 +0300

    мобильное меню-бургер

--- header.php
+++ header.php
@@ -12,38 +12,42 @@
       
       <header class="header">
         <div class="header__container _container">
-            <!-- <div class="logo"> -->
-            <?php
-                $args = array(
-                    'container'       => 'nav',          
-                    'container_class' => 'header__menu menu',           
-                    'menu_class'      => 'menu__list',          
-                    'fallback_cb'     => 'wp_page_menu',            
-                    'link_class'     => 'menu__link',           
-                    'theme_location'  => 'main_menu',
-                    'add_li_class'    => 'menu__item'
-                );
-                wp_nav_menu($args);
-            ?>
-        <div class="header__logo">
-            <div class="logo_img"><?php the_custom_logo() ?></div>
-            <div class="logo_text">top land</div>  
-        </div>
-       
-          <div class="header__location">
-            <div class="header__location_img"><img src="<?php echo get_template_directory_uri()?>/static/img/location.svg" alt="location"/></div>
-            <div class="header__location_text">Ростов-на-Дону</div>
+          <div class="header_wrapper">
+              <div class="header__burger">
+                <span></span>
+              </div>
+              <?php
+                  $args = array(
+                      'container'       => 'nav',          
+                      'container_class' => 'header__menu menu',           
+                      'menu_class'      => 'menu__list',          
+                      'fallback_cb'     => 'wp_page_menu',            
+                      'link_class'     => 'menu__link',           
+                      'theme_location'  => 'main_menu',
+                      'add_li_class'    => 'menu__item'
+                  );
+                  wp_nav_menu($args);
+              ?>
+          <div class="header__logo">
+              <div class="logo_img"><?php the_custom_logo() ?></div>
+              <div class="logo_text">top land</div>  
           </div>
+        
+            <div class="header__location">
+              <div class="header__location_img"><img src="<?php echo get_template_directory_uri()?>/static/img/location.svg" alt="location"/></div>
+              <div class="header__location_text">Ростов-на-Дону</div>
+            </div>
 
-          <div class="header__hrVert"></div>
+            <div class="header__hrVert"></div>
 
-          <div class="header__contact">
-            <div class="header__contact_telefon"><a href="tel:+79934536307">+7 993 453-63-07</div>
-            <div class="header_contact_href"><a class="header__link" href="https://wa.me/79934536307">Написать в What’sApp</a></div>
+            <div class="header__contact">
+              <div class="header__contact_telefon"><a href="tel:+79934536307">+7 993 453-63-07</a></div>
+              <div class="header_contact_href"><a class="header__link" href="https://wa.me/79934536307">Написать в What’sApp</a></div>
+            </div>
+            
           </div>
-          
+          <hr class="header__hr">
         </div>
-        <hr class="header__hr">
       </header>
 
         <main class="page">
--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,10 @@ $(document).ready(function () {
         $(this).toggleClass('open');
     });
 });
+
+$(document).ready(function () {
+    $('.header__burger').click(function (event) { 
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('main').toggleClass('lock')
+    })
+});
