f116ed2f519ab517bbbf3f3e3f90255e591bddd3
DiPokemon <dimitrnklnk@gmail.com>
2022-10-24 03:16:30 +0300

    full adaptive header, burger menu, blur mob menu

--- static/js/script.js
+++ static/js/script.js
@@ -2,11 +2,10 @@ $(document).ready(function () {
     $('.accordion__item').click(function () {
         $(this).toggleClass('open');
     });
+
+    $('.header__burger').click(function (event) {
+        $('.header__burger,.header__menu').toggleClass('active');
+        $('body').toggleClass('lock')
+    });
 });
 
-$(document).ready(function () {
-    $('.header__burger').click(function (event) { 
-        $('.header__burger,.header__menu').toggleClass('active');
-        $('main').toggleClass('lock')
-    })
-});
