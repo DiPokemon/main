70ca9496d7e05dfc17937c8249782ab68f85488f
DiPokemon <dimitrnklnk@gmail.com>
2022-10-28 23:57:39 +0300

    emptyfix

--- header.php
+++ header.php
@@ -30,9 +30,23 @@
                 'itemscope' => 'itemscope',
                 'itemtype'  => 'http://schema.org/SiteNavigationElement',
               ),     
-              'items_wrap'  => '<ul itemprop="about" itemscope="" itemtype="http://schema.org/ItemList" id="%1$s" class="%2$s">%3$s</ul>'               
+              'items_wrap'  => '<ul itemprop="about" itemscope="" itemtype="http://schema.org/ItemList" id="%1$s" class="%2$s">%3$s</ul>',
+              echo          => false,               
             );
-            wp_nav_menu($args);
+            $temp_menu = wp_nav_menu($args);
+            $temp_menu = str_replace("<a", "<a itemprop='url' ", $temp_menu);
+            foreach($matchesz[0] as $value){
+              if(strpos($value, "<span") === false){
+                $temp_value = preg_replace("~<a (.*?)>(.*)</a>~", "<a $1><span itemprop='name'>$2</span></a>", $value);
+
+                $temp_menu = str_replace($value, $temp_value, $temp_menu);
+              }else{
+                $temp_value = str_replace("<span", "<span itemprop='name'", $value);
+
+                $temp_menu = str_replace($value, $temp_value, $temp_menu);
+              }
+            }
+            echo $temp_menu;
           ?>
           <div class="header__logo">
             <div class="logo_img"><?php the_custom_logo() ?></div>
