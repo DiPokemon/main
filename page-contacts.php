7a8737d39a384af6c8993a2060b76ad05a47e562
DiPokemon <dimitrnklnk@gmail.com>
2022-10-30 21:55:52 +0300

    add map in contact page

--- page-contacts.php
+++ page-contacts.php
@@ -16,11 +16,11 @@
         </div>
     </section>
 
-    <section class="page__contacts-map">
+    <section class="page__contacts-map contact_block">
         <div class="page__contacts__container _container">
             <div class="page__contacts_map-wrapper">                
-                <div id="map_container" class="map container-fluid">                    
-                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A35c48d9808a50de74447c6d0ec48ba8b04881953d7f20f645b6e374170bb77f0&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>                   
+                <div id="map_container" class="map container-fluid">   
+                    <script id="ymap_lazy" type="text/javascript" charset="utf-8" async data-src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A35c48d9808a50de74447c6d0ec48ba8b04881953d7f20f645b6e374170bb77f0&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>                                                    
                 </div>
                 <div class="form">
                     <h3 class="form_title">У Вас остались вопросы</h3>
