59d218ce37cd371d32a7e30421e67c2727bed975
DiPokemon <dimitrnklnk@gmail.com>
2022-10-30 21:28:25 +0300

    alt's and title on pages

--- category-services.php
+++ category-services.php
@@ -44,11 +44,14 @@
                                             $post_thumbnail_url = get_template_directory_uri().'/static/empty-banner.gif';
                                         else
                                             $post_thumbnail_url = get_the_post_thumbnail_url();
+                                        $image_id = get_post_thumbnail_id();
+                                        $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
+                                        $image_title = get_the_title($image_id);
 
                                 ?>
                                     <a class="cases-block__slide case_slide" href="<?php the_permalink() ?>">
                                         <div class="case_slide_wrapper">
-                                        <img loading="lazy" src="<?= $post_thumbnail_url ?>" alt="<?php the_title() ?>">
+                                        <img loading="lazy" src="<?= $post_thumbnail_url ?>" alt="<?php echo $image_alt ?>" title="<?php echo $image_title ?>">
                                         <div class="case_slide_title_wrapper">
                                             <h3 class="case_slide__title"><?php the_title() ?></h3>
                                         </div>                                
