a94600fc2420e6cc1039598aff66022f432fd57f
DiPokemon <dimitrnklnk@gmail.com>
2022-11-01 01:07:02 +0300

    empty

--- header.php
+++ header.php
@@ -78,7 +78,7 @@
           <div class="header__hrVert"></div>
           <div class="header__contact">
             <div class="header__contact_telefon"><a href="tel:+79934536307">+7 993 453-63-07</a></div>
-            <div class="header__contact_telefon-mob"><a href="tel:+79934536307"><img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/phone.svg" /></a></div>
+            <div class="header__contact_telefon-mob"><a href="tel:+79934536307"><img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/phone.svg" alt="Телефон маркетингового агентства в Ростове"/></a></div>
             <div class="header_contact_href"><a class="header__link" href="https://wa.me/79934536307">Написать в What’sApp</a></div>
           </div>
             
