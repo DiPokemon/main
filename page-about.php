71fb77df65043d0e2f627d4dd134f708c33c8d8d
DiPokemon <dimitrnklnk@gmail.com>
2022-11-08 18:50:01 +0300

    add page-about

---
+++ page-about.php
@@ -0,0 +1,193 @@
+<?php get_header(); ?>
+
+<div class="_container">
+    <section class="page-header">
+        <?php if ( function_exists( 'topland_breadcrumbs' ) ) topland_breadcrumbs(); ?>        
+        <h1 class="page-header__title"><?php the_title(); ?></h1>
+    </section>        
+
+    <section class="page__about first_block_about">
+        <div class="_container">
+            <p class="about_subtitle">Миссия нашего маркетингового агентства - обеспечить старт Вашему бизнесу, сохранить или поднять занимаемые вами позиции.</p>
+            <div class="page__about_wrapper">
+
+                <div class="page__about_wrapper-left">
+
+                    <div class="about_item">
+                        <span class="about_item-number">01</span> 
+                        <div class="about_item-text"> 
+                            Агентство TopLand оказываем услуги интернет маркетинга с 2016 года
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">02</span> 
+                        <div class="about_item-text"> 
+                            Мы выполнили  более 70 успешных SEO проектов
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">03</span> 
+                        <div class="about_item-text"> 
+                            По показателю SEO выводим сайты клиентов на 70-90% в ТОП-10
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">04</span> 
+                            Повышаем продажи партнерских товаров и услуг в среднем в 3 раза
+                        <div class="about_item-text"> 
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">05</span> 
+                            Успешно реализовано более 15 проектов по созданию корпоративных сайтов
+                        <div class="about_item-text"> 
+                        </div>           
+                    </div>
+                </div>
+
+                <div class="page__about_wrapper-right">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_first.png" alt="Маркетинговое агентство в Ростове-на-Дону">
+                </div>
+        </div>
+    </section>
+
+    <section class="page__about second_block_about">
+        <div class="_container">
+            <h2>Что мы умеем</h2>
+            <div class="page__about_wrapper">
+
+                <div class="page__about_wrapper-left">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_second.png" alt="SEO продвижение сайтов в Ростове">
+                </div>
+                    
+                <div class="page__about_wrapper-right">
+                    
+
+                    <div class="about_item">
+                        <span class="about_item-number">01</span> 
+                        <div class="about_item-text"> 
+                            Разработка сайтов любой сложности с удобным интерфейсом, эстетичным внешним видом и практичным функционалом
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">02</span> 
+                        <div class="about_item-text"> 
+                            Seo продвижение: по позициям, тематике, KPI, оффлайн магазинов, сайтов услуг и т.д.
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">03</span> 
+                        <div class="about_item-text"> 
+                            Работа с маркетплейсом Wildberries: отзывы, контент, продвижение карточки товара в ТОП-10
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">04</span> 
+                            Гео продвижение: размещение на картах Google Maps, Яндекс Картах и 2GIS
+                        <div class="about_item-text"> 
+                        </div>           
+                    </div>                  
+                
+                </div>
+        </div>
+    </section>
+
+    <section class="page__about thrid_block_about">
+        <div class="_container">
+            <h2>Преимущества работы с нами</h2>
+            <div class="page__about_wrapper">
+
+                <div class="page__about_wrapper-left">
+                    
+                    <div class="about_item">
+                        <span class="about_item-number">01</span> 
+                        <div class="about_item-text"> 
+                            Способствуем расширению сферы деятельности партнеров благодаря правильной маркетинговой стратегии
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">02</span> 
+                        <div class="about_item-text"> 
+                            Работаем с технически сложными (узкоспециализированными) тематиками
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">03</span> 
+                        <div class="about_item-text"> 
+                            Помогаем снизить затраты на рекламные бюджеты в 2 раза за 4 месяца работы при сохранении эффективности рекламной кампании
+                        </div>           
+                    </div>
+
+                    <div class="about_item">
+                        <span class="about_item-number">04</span> 
+                            Публикуем результаты своих  работ с подробным  описанием процесса в разделе кейсы
+                        <div class="about_item-text"> 
+                        </div>           
+                    </div>      
+                    
+                    <div class="about_item">
+                        <span class="about_item-number">05</span>                             
+                        <div class="about_item-text"> 
+                            Комплексная работа над проектом
+                        </div>           
+                    </div>  
+                    
+                    <div class="about_item">
+                        <span class="about_item-number">05</span>                             
+                        <div class="about_item-text"> 
+                            Прозрачные отчеты и консультирование
+                        </div>           
+                    </div>  
+
+                    <div class="about_item">
+                        <span class="about_item-number">06</span>                             
+                        <div class="about_item-text"> 
+                        Решение поставленных задач и возможность дальнейшего развития
+                        </div>           
+                    </div> 
+                </div>
+                    
+                <div class="page__about_wrapper-right">
+                    <img loading="lazy" src="<?php echo get_template_directory_uri()?>/static/img/about_thrid.png" alt="SEO продвижение сайтов в Ростове">
+                </div>
+        </div>
+        <p class="about_subtitle">Маркетинговое агентство “TopLand” в Ростове на Дону, готово оказать вам услуги SEO продвижение вашего сайта под ключ. Для оформления заказа и консультации достаточно просто заполнить форму обратной связи.</p>
+    </section>
+
+    <section class="page__about">
+        <div class="page__about__container _container">
+            <div class="page__contacts__body">                
+                <div class="page__contacts_text">
+                    <?php the_content(); ?>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section class="page__contacts-map contact_block">
+        <div class="page__contacts__container _container">
+            <div class="page__contacts_map-wrapper">                
+                <div id="map_container" class="map container-fluid">   
+                    <script id="ymap_lazy" type="text/javascript" charset="utf-8" async data-src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A35c48d9808a50de74447c6d0ec48ba8b04881953d7f20f645b6e374170bb77f0&amp;width=100%25&amp;height=500&amp;lang=ru_RU&amp;scroll=true"></script>                                                    
+                </div>
+                <div class="form">
+                    <h3 class="form_title">У Вас остались вопросы</h3>
+                    <p class="form_text">Заполните форму обратной связи и наш специалист перезвонит Вам и проконсультирует по любой, интересующей Вас теме.</p>
+                    <?php echo do_shortcode('[contact-form-7 id="1968" title="Контактная форма 1"]'); ?>
+
+                </div>
+            </div>
+        </div>
+    </section>        
+</div>
+
+<?php get_footer(); ?>
