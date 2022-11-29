bb081f4c24073231dffd27e72e5955bfaab82b11
Dimitrniy Nikolenko <dimitrnklnk@gmail.com>
2022-11-29 23:17:44 +0300

    del plugins dir in template

--- plugins/topland_ymap1/views/replace_shortcode.php
+++
@@ -1,8 +0,0 @@
-<ul class="social">
-<?php
-	foreach ( self::$model->get_list() as $item ): ?>
-        <li>
-        	<a href="<?= $item->url ?>" title="<?= $item->name ?>" target="_blank"><img src="<?= self::$model->get_image_attachment_filepath($item->image_attachment_id) ?>"></a>
-        </li>
-<?php endforeach ?>
-</ul>
