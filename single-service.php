11cc4c87d730f79bf42dcd76d0c7b129f22b9e2d
Timur-tiger <timahusamov@mail.ru>
2023-06-04 18:39:38 +0300

    empty

--- single-service.php
+++ single-service.php
@@ -94,65 +94,6 @@ $cloud_tags = get_field("cloud_tags", $term);
                             </div>                        
                         </div>
                     </div>
-
-
-
-
-
-                    <div class="cases-block__container">
-                        <div class="cases-block__body">
-                        <div class="cases-block__title">
-                            <h2 class="_h2">Кейсы</h2>
-                        </div>
-                        <?php if (!empty($cases_text)): ?>
-                            <div class="cases_text service_text-block">
-                                <?= $cases_text ?>
-                            </div>   
-                        <?php endif;?>  
-                        <div class="slider_wrapper">
-                        <div class="cases_slider">
-                                    <?php $length_cases = 0 ?>
-                                    <?php
-                                        $args_for_cases = [
-                                            'posts_per_page' => -1,
-                                            'category_name'  => 'cases',
-                                            'offset'         => 0,
-                                        ];
-                                        $query_cases = new WP_Query( $args_for_cases );
-                                        while ($query_cases->have_posts()) :
-                                            $query_cases->the_post();
-                                            $length_cases++;
-                                            if (is_null(get_the_post_thumbnail_url()) || empty(get_the_post_thumbnail_url()))
-                                                $post_thumbnail_url = get_template_directory_uri().'/static/empty-banner.gif';
-                                            else
-                                                $post_thumbnail_url = get_the_post_thumbnail_url();
-                                            $image_id = get_post_thumbnail_id();
-                                            $image_alt = get_post_meta($image_id, '_wp_attachment_image_alt', TRUE);
-                                            $image_title = get_the_title($image_id);
-                                    ?>
-                                        <a class="cases-block__slide case_slide" href="<?php the_permalink() ?>">
-                                            <div class="case_slide_wrapper">
-                                            <img loading="lazy" src="<?= $post_thumbnail_url ?>" alt="<?php echo $image_alt ?>" title="<?php echo $image_title ?>">
-                                            <div class="case_slide_title_wrapper">
-                                                <h3 class="case_slide__title"><?php the_title() ?></h3>
-                                            </div>                                
-                                            </div>                              
-                                        </a>
-                                    <?php endwhile; wp_reset_query(); ?>  
-                            </div>
-                            <div class="slider-controls">
-                                <button type="button" class="slide-m-prev"></button>
-                                <div class="slide-m-dots"></div>
-                                <button type="button" class="slide-m-next"></button>
-                            </div>
-                        </div>
-                        </div>
-                                          
-                    </div>  
-                                    
-
-                                        
-
         </section>
 
 
